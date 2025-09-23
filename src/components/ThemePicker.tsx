6639f6c52a7912db6c7eb76515ddc31ec6ac9bbd
Fadi Khadra <fdkhadra@gmail.com>
2025-09-22 22:27:57 -0400

    feat: add persistance to theme picker

---
+++ src/components/ThemePicker.tsx
@@ -0,0 +1,67 @@
+import { MonitorCog, Moon, Sun } from "lucide-react";
+import { useState } from "react";
+
+type Themes = "light" | "dark" | "system";
+
+export function ThemePicker() {
+  const [theme, setTheme] = useState<Themes>(() => {
+    return (localStorage.getItem("theme") as Themes) || "system";
+  });
+
+  return (
+    <fieldset>
+      <legend className="sr-only">Select a theme</legend>
+
+      <div className="grid grid-cols-3 gap-4">
+        {["light", "dark", "system"].map((v) => (
+          <label
+            key={v}
+            htmlFor={v}
+            className="has-checked:border-primary hover:has-checked:border-primary dark:bg-input bg-background has-checked:bg-primary/15 cursor-pointer rounded-md border-2 border-gray-300 p-4 hover:border-slate-400 dark:border-gray-500 dark:hover:border-slate-50"
+          >
+            <input
+              type="radio"
+              className="peer absolute appearance-none"
+              id={v}
+              value={v}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  switch (v) {
+                    case "light":
+                      document.documentElement.classList.toggle("dark", false);
+                      localStorage.setItem("theme", "light");
+                      break;
+                    case "dark":
+                      document.documentElement.classList.toggle("dark", true);
+                      localStorage.setItem("theme", "dark");
+                      break;
+                    case "system":
+                    default:
+                      document.documentElement.classList.toggle(
+                        "dark",
+                        localStorage.theme === "dark" ||
+                          (!("theme" in localStorage) &&
+                            window.matchMedia("(prefers-color-scheme: dark)")
+                              .matches),
+                      );
+                      localStorage.setItem("theme", "system");
+                      break;
+                  }
+
+                  setTheme(v as Themes);
+                }
+              }}
+              checked={v === theme}
+            />
+            <div className="peer-checked:text-primary flex flex-col items-center gap-2 text-center capitalize">
+              {v === "light" && <Sun />}
+              {v === "dark" && <Moon />}
+              {v === "system" && <MonitorCog />}
+              <span>{v}</span>
+            </div>
+          </label>
+        ))}
+      </div>
+    </fieldset>
+  );
+}
--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,14 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Keyboard, MonitorCog, Moon, Palette, Power, Sun } from "lucide-react";
-import { useState } from "react";
+import { Keyboard, Palette, Power } from "lucide-react";
 import { FormControl, Input, Label, Switch } from "~/components/Form";
 import { Keybind } from "~/components/Keybind";
+import { ThemePicker } from "~/components/ThemePicker";
 import { Typography } from "~/components/Typography";
 
 export const Route = createFileRoute("/settings")({
   component() {
-    const [theme, setTheme] = useState("light");
-
     return (
       <section id="setting-page" className="flex h-dvh flex-col p-4">
         <div className="flex h-full flex-col gap-7">
@@ -30,59 +28,7 @@ export const Route = createFileRoute("/settings")({
               <Palette className="mr-2" />
               Theme
             </Typography.h4>
-            <fieldset>
-              <legend className="sr-only">Select a theme</legend>
-
-              <div className="grid grid-cols-3 gap-4">
-                {["light", "dark", "auto"].map((v) => (
-                  <label
-                    key={v}
-                    htmlFor={v}
-                    className="has-checked:border-primary hover:has-checked:border-primary dark:bg-input bg-background has-checked:bg-primary/15 cursor-pointer rounded-md border-2 border-gray-300 p-4 hover:border-slate-400 dark:border-gray-500 dark:hover:border-slate-50"
-                  >
-                    <input
-                      type="radio"
-                      className="peer absolute appearance-none"
-                      id={v}
-                      value={v}
-                      onChange={(e) => {
-                        if (e.target.checked) {
-                          if (v === "light") {
-                            document.documentElement.classList.toggle(
-                              "dark",
-                              false,
-                            );
-                          } else if (v === "dark") {
-                            document.documentElement.classList.toggle(
-                              "dark",
-                              true,
-                            );
-                          } else {
-                            document.documentElement.classList.toggle(
-                              "dark",
-                              localStorage.theme === "dark" ||
-                                (!("theme" in localStorage) &&
-                                  window.matchMedia(
-                                    "(prefers-color-scheme: dark)",
-                                  ).matches),
-                            );
-                          }
-
-                          setTheme(v);
-                        }
-                      }}
-                      checked={v === theme}
-                    />
-                    <div className="peer-checked:text-primary flex flex-col items-center gap-2 text-center capitalize">
-                      {v === "light" && <Sun />}
-                      {v === "dark" && <Moon />}
-                      {v === "auto" && <MonitorCog />}
-                      <span>{v}</span>
-                    </div>
-                  </label>
-                ))}
-              </div>
-            </fieldset>
+            <ThemePicker />
           </section>
           <div className="h-[1px] bg-pink-400/10" />
           <section>
