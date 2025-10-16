628185d9bbad975f2f8f0aa9ace19cedbeea542c
Fadi Khadra <fdkhadra@gmail.com>
2025-10-15 22:02:51 -0400

    update typing

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,25 @@
 export type { Repository } from "./homepage";
 export * from "./rest";
 export type {
-  PullRequest,
-  PullRequestReviewDecision,
-  PullRequestState,
+	PullRequest,
+	PullRequestReviewDecision,
+	PullRequestState,
 } from "./search-pull-request";
+export type { Theme } from "./settings";
+export * as settings from "./settings";
 export type {
-  Organization,
-  UserProfileViewer as UserProfile,
+	Organization,
+	UserProfileViewer as UserProfile,
 } from "./user-profile";
+
+import type * as events from "./events";
+
+export type AuthenticatedPayload = Extract<
+	events.Event,
+	{ authenticated: unknown }
+>;
+
+export type ThemeChangedPayload = Extract<
+	events.Event,
+	{ themeChanged: unknown }
+>;
