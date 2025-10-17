5e6dc93c8a5327de9dfd1eae72932af80c464bbc
Fadi Khadra <fdkhadra@gmail.com>
2025-10-16 21:21:41 -0400

    feat: add p search params for breadcrumb

--- src/routes/palette/route.tsx
+++ src/routes/palette/route.tsx
@@ -9,54 +9,62 @@ import { useFullHeightRef } from "~/libs/useFullHeight";
 import { Navigation } from "./-Navigation";
 
 export const Route = createFileRoute("/palette")({
-  component,
+	component,
+	validateSearch(p): { p: string | undefined } {
+		return {
+			p: p.p as string,
+		};
+	},
 });
 
 function component() {
-  const listBox = useFullHeightRef<HTMLDivElement>({ bottomPadding: 52 });
-  const isGhSearchActive = useGhSearchActive();
+	const listBox = useFullHeightRef<HTMLDivElement>({ bottomPadding: 52 });
+	const isGhSearchActive = useGhSearchActive();
 
-  usePreloadRoutes();
+	usePreloadRoutes();
 
-  return (
-    <div
-      className={"h-screen bg-gradient-to-tl from-pink-300/10 to-purple-500/10"}
-    >
-      <Navigation />
-      <Command
-        loop
-        className="relative h-full"
-        shouldFilter={!isGhSearchActive}
-      >
-        <CommandInput />
-        <Command.List ref={listBox.setRef} className="px-2">
-          <EmptySearchResults />
-          <Outlet />
-        </Command.List>
-        <Keybinds />
-      </Command>
-    </div>
-  );
+	return (
+		<div
+			className={"h-screen bg-gradient-to-tl from-pink-300/10 to-purple-500/10"}
+		>
+			<Navigation />
+			<Command
+				loop
+				className="relative h-full overflow-hidden"
+				shouldFilter={!isGhSearchActive}
+			>
+				<CommandInput />
+				<Command.List ref={listBox.setRef} className="px-2">
+					<EmptySearchResults />
+					<Outlet />
+				</Command.List>
+				<Keybinds />
+			</Command>
+		</div>
+	);
 }
 
 function EmptySearchResults() {
-  const search = useCommandState((s) => s.search);
-  const isGhSearchActive = useGhSearchActive();
+	const search = useCommandState((s) => s.search);
+	const isGhSearchActive = useGhSearchActive();
+	// const isPending = useRouterState({
+	// 	select: (s) => s.status === "pending",
+	// });
 
-  if (isGhSearchActive || search.length === 0) return null;
+	if (isGhSearchActive || search.length === 0) return null;
 
-  return (
-    <CommandEmpty>
-      <>
-        <span
-          role="img"
-          aria-label="dunno what you are looking for"
-          className="mr-1"
-        >
-          🤷‍♂️
-        </span>
-        No results found.
-      </>
-    </CommandEmpty>
-  );
+	// if (isPending) return <span>LOADING FTW</span>;
+
+	return (
+		<CommandEmpty>
+			<span
+				role="img"
+				aria-label="dunno what you are looking for"
+				className="mr-1"
+			>
+				🤷‍♂️
+			</span>
+			No results found.
+		</CommandEmpty>
+	);
 }
