0fbd920eb3036372efac5bb0f989f2181bd70679
Fadi Khadra <fdkhadra@gmail.com>
2025-10-18 21:52:35 -0400

    refactor: use selector

--- src/features/palette/useGhSearchActive.ts
+++ src/features/palette/useGhSearchActive.ts
@@ -1,6 +1,6 @@
 import { useLocation } from "@tanstack/react-router";
 
 export function useGhSearchActive() {
-  const location = useLocation();
-  return location.pathname.startsWith("/palette/search");
+	const pathname = useLocation({ select: (s) => s.pathname });
+	return pathname.startsWith("/palette/search");
 }
