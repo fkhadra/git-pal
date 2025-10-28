e102d23036580049686fc5e387861b83f0a5ce68
Fadi Khadra <fdkhadra@gmail.com>
2025-10-27 20:34:47 -0400

    switch to min height and fix dependency

--- src/libs/useFullHeight.ts
+++ src/libs/useFullHeight.ts
@@ -1,26 +1,25 @@
 import { useCallback, useRef } from "react";
 
 export type NodeRef<T extends HTMLElement> = ReturnType<
-  typeof useFullHeightRef<T>
+	typeof useFullHeightRef<T>
 >;
 
 export function useFullHeightRef<T extends HTMLElement>(
-  {
-    bottomPadding = 0,
-  }: {
-    bottomPadding?: number;
-  } = { bottomPadding: 0 },
+	{ bottomPadding = 0 }: { bottomPadding?: number } = { bottomPadding: 0 },
 ) {
-  const nodeRef = useRef<T>(null);
-  const setRef = useCallback((el: T) => {
-    if (el) {
-      nodeRef.current = el;
-      el.style.height = `calc(100dvh - ${el.getBoundingClientRect().top + bottomPadding}px)`;
-    }
-  }, []);
+	const nodeRef = useRef<T>(null);
+	const setRef = useCallback(
+		(el: T) => {
+			if (el) {
+				nodeRef.current = el;
+				el.style.minHeight = `calc(100dvh - ${el.getBoundingClientRect().top + bottomPadding}px)`;
+			}
+		},
+		[bottomPadding],
+	);
 
-  return {
-    setRef,
-    node: nodeRef.current,
-  };
+	return {
+		setRef,
+		node: nodeRef.current,
+	};
 }
